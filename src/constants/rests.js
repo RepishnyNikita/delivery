50331faebe3781847dbf74462df94a877673056f
RepishnyNikita <armyrepa@mail.ru>
2023-12-26 08:22:21 +0300

    fix

---
+++ src/constants/rests.js
@@ -0,0 +1,116 @@
+export const restArray = [
+    {
+      id: 0,
+      title: "Пицца плюс",
+      time: 50,
+      rating: 4.5,
+      price: 900,
+      type: "Пицца",
+      img: "pizza-plus.jpg",
+    },
+  
+    {
+      id: 1,
+      title: "Тануки",
+      time: 50,
+      rating: 4.5,
+      price: 900,
+      type: "Пицца",
+      img: "tanuki.jpg",
+    },
+  
+    {
+      id: 2,
+      title: "FoodBand",
+      time: 50,
+      rating: 4.5,
+      price: 900,
+      type: "Пицца",
+      img: "foodband.jpg",
+    },
+  
+    {
+      id: 3,
+      title: "Жадина-пицца",
+      time: 50,
+      rating: 4.5,
+      price: 900,
+      type: "Пицца",
+      img: "greedy-pizza.jpg",
+    },
+  
+    {
+      id: 4,
+      title: "Точка еды",
+      time: 50,
+      rating: 4.5,
+      price: 900,
+      type: "Пицца",
+      img: "foodoutlet.jpg",
+    },
+  
+    {
+      id: 5,
+      title: "PizzaBurger",
+      time: 50,
+      rating: 4.5,
+      price: 900,
+      type: "Пицца",
+      img: "pizza-burger.jpg",
+    },
+  ];
+
+export const goodsArray = [
+  {
+    id: 0,
+    name: "Ролл угорь стандарт",
+    description: "Рис, угорь, соус унаги, кунжут, водоросли нори.",
+    price: 250,
+    img: "eel.jpg",
+  },
+
+  {
+    id: 1,
+    name: "Калифорния лосось стандарт",
+    description:
+      "Рис, лосось, авокадо, огурец, майонез, икра масаго, водоросли нори",
+    price: 250,
+    img: "colifornia.jpg",
+  },
+
+  {
+    id: 2,
+    name: "Окинава стандарт",
+    description:
+      "Рис, креветка отварная, сыр сливочный, лосось, огурец свежий",
+    price: 250,
+    img: "okinava.jpg",
+  },
+
+  {
+    id: 3,
+    name: "Цезарь маки хl",
+    description:
+      "Рис, куриная грудка копченая, икра масаго, томат, айсберг, соус цезарь",
+    price: 250,
+    img: "сaesar.jpg",
+  },
+
+  {
+    id: 4,
+    name: "Ясай маки стандарт 185 г",
+    description:
+      "Рис, помидор свежий, перец болгарский, авокадо, огурец, айсберг",
+    price: 250,
+    img: "maki.jpg",
+  },
+
+  {
+    id: 5,
+    name: "Ролл с креветкой стандарт",
+    description:
+      "Рис, водоросли нори, креветки отварные, сыр сливочный, огурцы",
+    price: 250,
+    img: "roll-shrimp.jpg",
+  },
+];
---
+++ src/views/HomeView.vue
@@ -0,0 +1,73 @@
+<template>
+  <section class="banner">
+    <div class="container">
+      <div class="banner-item">
+        <h2 class="banner-item--title">
+          Онлайн-сервис<br />доставки еды на дом
+        </h2>
+        <p class="banner-item--subtitle">
+          Блюда из любимого ресторана привезет курьер в перчатках, маске и с
+          антисептиком
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <section class="products">
+    <div class="container">
+      <div class="products-header">
+        <h3 class="products-header--title">Рестораны</h3>
+
+        <input
+          type="text"
+          class="products-header--search"
+          placeholder="Поиск блюд и ресторанов"
+        />
+      </div>
+
+      <div id="rests-container" class="products-wrapper">
+        <router-link
+          class="product-card"
+          v-for="rest in restArray"
+          :key="rest.id"
+          :to="`/rest?id=${rest.id}`"
+        >
+          <div class="product-card__img">
+            <img :src="getUrl(rest.img)" alt="pizza-plus" />
+          </div>
+          
+          <div class="product-card__description">
+            <div class="product-card__description-row">
+              <h4 class="product-card__description--title">{{ rest.title }}</h4>
+              <div class="product-card__description-badge">{{ rest.time }}</div>
+            </div>
+
+            <div class="product-card__description-row">
+              <div class="product-card__description-info">
+                <div class="product-card__description-info--rairing">
+                  <img src="./../assets/img/icon/star.svg" alt="star" />
+                  {{ rest.rating }}
+                </div>
+
+                <div class="product-card__description-info--price">
+                  {{ rest.price }} ₽
+                </div>
+
+                <div class="product-card__description-info--gruop">
+                  {{ rest.type }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </router-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import {restArray} from './../constants/rests'
+const getUrl = (name) => {
+  return new URL(`../assets/img/rest/${name}`, import.meta.url)
+};
+</script>
